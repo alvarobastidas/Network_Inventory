a13448229403d433332fc327c97d0840fbd369d6
Alvaro <alvfer28@gmail.com>
2025-09-10 08:10:29 +0100

    Update project

---
+++ scripts/example.py
@@ -0,0 +1,116 @@
+import json
+import yaml
+import paramiko
+import time
+from netmiko import ConnectHandler
+
+json_path = '/Users/fernando/Desktop/Alvaro/Personal/Study-Guides-Cert/Cisco/Python-Labs/CiscoTest/resources' \
+            '/questions.json'
+
+with open(json_path, 'r') as f:
+    questions = json.load(f)  # convert a JSON file to Python Object ( List of Dictionnaries)
+
+
+for item in questions:
+    print('************ DICTIONNARY to JSON **************')
+    print(json.dumps(item, indent=4)) # convert a Python Dictionnary to JSON string and print in json format
+    a = json.dumps(item, indent=4)
+    print('************     DICTIONNARY to YAML    **************')
+    print(yaml.dump(item, indent=4, sort_keys=False)) # convert Python Dictionnary to YAML string and print YAML format
+    break
+
+
+
+with open ('data.yaml', 'w') as file:
+    yaml.dump(questions, file, sort_keys=False)
+
+a = {"name": "Diana", "age": 37, "country": "Ecuador"}
+# a_json_str = json.dumps(a, indent=4)
+# print(a_json_str)
+
+b_yaml = yaml.dump(a, indent=4, sort_keys=False)
+print(b_yaml)
+
+
+# -----------Using PARAMIKO ----------------------- #
+# Create SSH Client
+client = paramiko.SSHClient()
+
+# Set Policy to automatically add host to known host file
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+# Connect to the remote host
+client.connect(
+    hostname="10.10.20.31",
+    username="alvaro",
+    password="2LNN%6^l2*bpa",
+    look_for_keys=False,
+    allow_agent=False,
+)
+
+# Open an interactive SSH session
+ssh_client = client.invoke_shell()
+
+# Send command
+ssh_client.send("sh ip int brief\n")
+
+# Wait for the command to be finished
+time.sleep(3)
+
+# Receive and process command output
+output = ssh_client.recv(65000)
+print(output.decode("ascii"))
+
+# Create Loopback interface
+print(" ########## Create Loopback Interface ###########")
+ssh_client.send("ena\n")
+ssh_client.send("C1sco12345\n")
+ssh_client.send("conf ter\n")
+
+for item in range(0, 1):
+    ssh_client.send(f"int lo {item}\n")
+    ssh_client.send(f"ip add 1.1.1.{item} 255.255.255.255\n")
+
+time.sleep(1)
+
+ssh_client.send("end\n")
+ssh_client.send("sh ip int brief\n")
+
+time.sleep(3)
+output = ssh_client.recv(65000)
+print(output.decode("ascii"))
+
+
+# Close the SSH session
+ssh_client.close()
+
+# Close the connection
+client.close()
+
+
+# -------------------Using NETMIKO ----------------------- #
+
+# Create device inventory in a dictionary
+SW_01 = {
+    "device_type": "cisco_ios",
+    "host": "10.10.20.31",
+    "username": "alvaro",
+    "password": "2LNN%6^l2*bpa",
+    "secret": "C1sco12345"
+}
+
+# Unpacking the dictionnary to connect device
+connection = ConnectHandler(**SW_01)
+connection.enable() #Enable method
+connection.config_mode() #Global config mode
+
+config_commands = ['ip access-list extended TEST', 'permit tcp any any eq www', 'permit tcp host 10.1.1.5 any eq ssh', 'exit', 'int lo 0', 'ip access-group TEST in', 'exit']
+connection.send_config_set(config_commands)
+
+connection.exit_config_mode()
+connection.send_command('wr')
+show_output = connection.send_command('sh run int l0')
+print(show_output)
+
+# close connection
+connection.disconnect()
---
+++ scripts/main.py
@@ -0,0 +1,170 @@
+import meraki
+import pandas as pd
+import json
+
+def get_inventory(dashboard): #Retrivev inventory for all accessible organizations
+   inventory = []
+   _inventory = []
+   _devices  = []
+   _networks = []
+   _licenses = []
+   _status   = []
+
+   try:
+      organizations = dashboard.organizations.getOrganizations()
+      for org in organizations:
+         devices  = dashboard.organizations.getOrganizationDevices(org["id"])
+
+         for dev in devices:
+            _devices.append(dev)
+            item = {}
+            item["organizationID"]   = org['id']
+            item["organizationName"] = org['name']
+            item['networkID']        = dev["networkId"]
+            item["deviceName"]       = dev["name"]
+            item["deviceSerial"]     = dev["serial"]
+            item["productType"]      = dev["productType"]
+            item["deviceModel"]      = dev["model"]
+            item["deviceAddress"]    = dev["address"]
+            if dev["productType"] in ("wireless", "switch"):
+               item["lanIp"]          = dev["lanIp"]
+               item["wan1Ip"]         = 'N/A'
+               item["wan2Ip"]         = 'N/A'
+            elif dev["productType"] == "appliance":
+               item["lanIp"]          = 'N/A'
+               item["wan1Ip"]         = dev["wan1Ip"]
+               item["wan2Ip"]         = dev["wan2Ip"]
+            else:
+               item["lanIp"]          = 'N/A'
+               item["wan1Ip"]         = 'N/A'
+               item["wan2Ip"]         = 'N/A'
+            inventory.append(item)
+
+      for org in organizations:
+         networks = dashboard.organizations.getOrganizationNetworks(org["id"])
+
+         for net in networks:
+            _networks.append(net)
+            for device in inventory:
+               if device["networkID"] == net["id"]:
+                  try:
+                     if device["networkName"]:
+                        print('Device Network already updated')
+                  except:
+                     device.update({
+                     'networkName': net["name"],
+                     'networkTime': net["timeZone"]
+                  })
+
+      for org in organizations:
+         licenses = dashboard.organizations.getOrganizationLicenses(org["id"])
+         for lic in licenses:
+            _licenses.append(lic)
+            for device in inventory:
+               if device["deviceSerial"] == lic["deviceSerial"]:
+                  device.update({
+                        "licenseType":    lic["licenseType"],
+                        "licenseState":   lic["state"],
+                        "activationDate": lic["activationDate"],
+                        "expirationDate": lic["expirationDate"]
+                     })
+               else:
+                  try:
+                     if device["licenseType"]:
+                        pass
+                        # print('Device License already updated')
+                  except:
+                     device.update({
+                        "licenseType": 'N/A',
+                        "licenseState": 'N/A',
+                        "activationDate": 'N/A',
+                        "expirationDate": 'N/A'
+                     })
+
+      for org in organizations:
+         status   = dashboard.organizations.getOrganizationDevicesStatuses(org["id"])
+         for stat in status:
+            _status.append(stat)
+            for device in inventory:
+               if device["deviceSerial"] == stat["serial"]:
+                  device.update({
+                     "devicePublicIp": stat["publicIp"],
+                     "deviceStatus":   stat["status"]
+                     })
+               else:
+                  try:
+                     if device["deviceStatus"]:
+                        pass
+                        # print('Device Status already updated')
+                  except:
+                     device.update({
+                        "devicePublicIp": "N/A",
+                        "deviceStatus":   "N/A"
+                     })
+
+      for inv in inventory:
+         _inventory.append({
+            "organizationID":    inv["organizationID"],
+            "organizationName":  inv["organizationName"],
+            "networkID":         inv["networkID"],
+            "networkName":       inv["networkName"],
+            "networkTime":       inv["networkTime"],
+            "deviceName":        inv["deviceName"],
+            "deviceSerial":      inv["deviceSerial"],
+            "productType":       inv["productType"],
+            "deviceModel":       inv["deviceModel"],
+            "deviceAddress":     inv["deviceAddress"],
+            "lanIp":             inv["lanIp"],
+            "wan1Ip":            inv["wan1Ip"],
+            "wan2Ip":            inv["wan2Ip"],
+            "licenseType":       inv["licenseType"],
+            "licenseState":      inv["licenseState"],
+            "activationDate":    inv["activationDate"],
+            "expirationDate":    inv["expirationDate"],
+            "devicePublicIp":    inv["devicePublicIp"],
+            "deviceStatus":      inv["deviceStatus"]
+         })
+
+   except Exception as e:
+      print(f"Error retrieving inventory: {e}")
+
+   return pd.DataFrame(_inventory), pd.DataFrame(organizations), pd.DataFrame(_devices), pd.DataFrame(_networks), pd.DataFrame(_licenses), pd.DataFrame(_status)
+
+
+# Replace with your Meraki Dashboard API Key
+API_KEY = '25cabf2e15201ff8d36af3be61ef5ddc07ed43f0'
+
+#Initializate the Meraki Dashboard API client
+_dashboard = meraki.DashboardAPI(API_KEY, suppress_logging=True)
+
+
+if __name__=="__main__":
+   df = get_inventory(_dashboard)
+   inventory      = df[0]
+   organizations  = df[1]
+   devices        = df[2]
+   networks       = df[3]
+   licenses       = df[4]
+   status         = df[5]
+    
+   # Save to Excel with multiple sheets
+   with pd.ExcelWriter("Meraki_inventory.xlsx", engine="openpyxl") as writer:
+      inventory.to_excel(writer, sheet_name="Inventory", index=False)
+      organizations.to_excel(writer, sheet_name="Organizations", index=False)
+      networks.to_excel(writer, sheet_name="Networks", index=False)
+      devices.to_excel(writer, sheet_name="Devices", index=False)
+      licenses.to_excel(writer, sheet_name="Licenses", index=False)
+      status.to_excel(writer, sheet_name="Status", index=False)
+
+   print("\n✅ Inventory saved to 'Meraki_inventory_with_lastSeen.xlsx' ")
+   print("      -Inventory sheet --> full inventory")
+   print("      -Organizations sheet --> Organizations list")
+   print("      -Networks sheet --> Networks list")
+   print("      -Devices sheet --> Devices list")
+   print("      -Licenses sheet --> Licenses Devices list")
+   print("      -Status sheet --> Status Devices list")
+
+else:
+   print("\n⚠️ No inventory data retrieved")
+   
+      
